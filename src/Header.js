3b6efe2677b3251c184e02d80efd8261a0ce8ef2
70gaurav <gk17265@gmail.com>
2023-03-22 18:27:27 +0530

    done

--- src/Header.js
+++ src/Header.js
@@ -7,6 +7,8 @@ import { Link } from 'react-router-dom'
      <header>
         <h1>Check your skills</h1>
         <Link to="/Register"><h2>Register</h2></Link>
+        <Link to="/"><h2>Login</h2></Link>
+        {/* <Link to="/Score"><h2>Score</h2></Link> */}
      </header>
    )
  }
--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,15 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react';
+import {  useNavigate } from 'react-router-dom';
+import { scoreContext } from './App';
 
 function Question() {
-  const [count,setcount] = useState(0)
+  const [count, setCount] = useState(0);
+  const {score , setScore} = useContext(scoreContext)
+  
+  const navigate = useNavigate()
+  
+
+  
   const questions = [
     {
       question: "Q1:What is the capital city of France?",
@@ -84,33 +92,71 @@ function Question() {
       answer: "a"
     }
   ]
-  
+
+
+  function handleAnswerSelect(e) {
+    const selectedAnswer = e.target.value
+    console.log(selectedAnswer)
+    const correctAnswer = questions[count].answer
+    if (selectedAnswer === correctAnswer) {
+      setScore(score + 1);
+    }
+  }
+
+  function renderQuestion() {
+    const currentQuestion = questions[count];
+    return (
+      <div>
+        <h1>{currentQuestion.question}</h1>
+        <ul>
+          <li>
+            <input type="radio" name="answer" id="ans1" value="a" onChange={handleAnswerSelect} />
+            <label htmlFor="ans1" id="option1">{currentQuestion.a}</label>
+          </li>
+          <li>
+            <input type="radio" name="answer" id="ans2" value="b" onChange={handleAnswerSelect} />
+            <label htmlFor="ans2" id="option2">{currentQuestion.b}</label>
+          </li>
+          <li>
+            <input type="radio" name="answer" id="ans3" value="c" onChange={handleAnswerSelect} />
+            <label htmlFor="ans3" id="option3">{currentQuestion.c}</label>
+          </li>
+          <li>
+            <input type="radio" name="answer" id="ans4" value="d" onChange={handleAnswerSelect} />
+            <label htmlFor="ans4" id="option4">{currentQuestion.d}</label>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+
+  // function renderResult() {
+  //   return (
+  //     <div>
+  //       <h2>Quiz completed!</h2>
+  //       <p>Your score: {score} / {questions.length}</p>
+  //     </div>
+  //   );
+  // }
+  function renderContent() {
+    if (count >= questions.length) {
+      // return renderResult();
+      navigate("/Score")
+
+    }
+    return renderQuestion();
+  }
+
   return (
     <div className='question'>
       <div className='content'>
-      <h1>{questions[count].question}</h1>
-      <ul>
-                    <li><input type="radio" name="answer"></input>
-                        <label for="ans1" id="option1">{questions[count].a}</label>
-                    </li>
-                    <li><input type="radio" name="answer"></input>
-                        <label for="ans2" id="option2">{questions[count].b}</label>
-                    </li>
-                    <li><input type="radio" name="answer" ></input>
-                        <label for="ans3" id="option3">{questions[count].c}</label>
-                    </li>
-                    <li><input type="radio" name="answer"></input>
-                        <label for="ans4" id="option4">{questions[count].d}</label>
-                    </li>
-
-                </ul>
-
-
-      <button onClick={() => setcount(count+1)}>Next</button>
+        {renderContent()}
+        {count < questions.length && (
+          <button onClick={() => setCount(count + 1)}>Next</button>
+        )}
       </div>
-
     </div>
-  )
+  );
 }
 
-export default Question
+export default Question;
